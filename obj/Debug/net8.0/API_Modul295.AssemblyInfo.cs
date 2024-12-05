316108b06d05e63b7d82489d15c61e768d39ead7
Maximilian Läpple <m.max32@yahoo.com>
2024-12-05 19:12:57 +0100

    Add dashboard.html - Change skript file

--- obj/Debug/net8.0/API_Modul295.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_Modul295.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Modul295")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54efec3a0e5a91201a513d728522a66c721b4f2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c5b1340cb79990da86015df117b3fe1b6abea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Modul295")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Modul295")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
