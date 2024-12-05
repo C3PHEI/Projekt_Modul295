165a311987cfae07444073aa318abbc7f125f2b7
Maximilian Läpple <m.max32@yahoo.com>
2024-12-05 18:23:22 +0100

    test

--- obj/Debug/net8.0/API_Modul295.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_Modul295.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Modul295")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f5a644ca648b89c0aed7466a64adba8902ae06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54efec3a0e5a91201a513d728522a66c721b4f2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Modul295")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Modul295")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
